77b5e47cc3cd735a31eea608f32c961f57ef66dc
esdrasjnl <esdrasnl@gmail.com>
2020-09-05 19:29:58 -0600

    Se creo el fichero usuario y operaciones

---
+++ Usuario.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author tu3
+ */
+public class Usuario {
+    private String Username;
+    private String Password;
+
+    public Usuario(){
+        this.Password="";
+        this.Username="";
+    }
+    
+    
+    /**
+     * @return the Username
+     */
+    public String getUsername() {
+        return Username;
+    }
+
+    /**
+     * @param Username the Username to set
+     */
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    /**
+     * @return the Password
+     */
+    public String getPassword() {
+        return Password;
+    }
+
+    /**
+     * @param Password the Password to set
+     */
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+}
